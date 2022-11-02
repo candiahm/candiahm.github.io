73eafad81016beec628468c771bbe6b40b0b189f
Martín <64814548+candiahm@users.noreply.github.com>
2022-11-02 11:14:32 -0300

    Reemplazo 20221102

--- sw.js
+++ sw.js
@@ -1,15 +1,51 @@
-// Tres eventos:
 
-// Instala el SW
-self.addEventListener("install", (evt) => {
-  console.log("SW Install..", evt);
+
+//activate
+
+
+/**
+ * 
+ * fetch
+ * 
+ * push
+ */
+
+self.addEventListener("install", (event)=> {
+    //console.log("install")
+    console.log('%c SW - install ', 'background: #222; color: #bada55');
+
+    //console.log("%cService Worker Instalado", 'color: green; background: yellow; font-size: 30px')
+
+
+    // El nuevo sw si la app se está ejecuntado
+    // queda en modo waiting hasta que el anterior se desactive
+    // Queda a la espera del cambio.
+    // Sirve en DEV en PROD no conviene. Pero el profe dice que él lo deja.
+    // Conclusión dejarlo
+    // Se utiliza con Clientes.claim()
+    // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerGlobalScope/skipWaiting
+    //
+    self.skipWaiting();
 })
 
-// El SW se activó
-self.addEventListener("activate", (evt) => {
-  console.log("SW Activate..", evt);
+self.addEventListener("activate", (event)=> {
+    //console.log("activate")
+    console.log('%c SW - activate ', 'background: #222; color: #bada55');
+
+    
 })
 
-self.addEventListener("fetch", (evt) => {
-  console.log("SW", "Fetch");
+self.addEventListener("fetch", (event)=> {
+    //console.log("fetch")
+    console.log('%c SW - fetch ', 'background: #222; color: #bada55');
+
+
+})
+
+self.addEventListener("push", (event)=> {
+    //console.log("SW - push", event)
+    console.log('%c SW - push ', 'background: #222; color: #bada55', event);
+    console.log('%c SW - push ', 'background: #222; color: #bada55', event.data.text());
+
+    serviceWorkerRegistration.showNotification(event.data.text())
 })
